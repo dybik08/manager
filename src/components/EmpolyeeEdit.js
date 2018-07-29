080c5b29d7fa34ad02599ede68d2004599ad4e2e
dybik08 <dybik08@gmail.com>
2018-07-29 13:57:04 +0200

    Add delete employee action and finish exercise

--- src/components/EmpolyeeEdit.js
+++ src/components/EmpolyeeEdit.js
@@ -27,6 +27,16 @@ class EmployeeEdit extends Component {
         Communications.text(phone,` Your upcoming shift is on ${shift}`);
     }
 
+    onAccept() {
+        const { uid } = this.props.employee;
+
+        this.props.employeeDelete({ uid });
+    }
+
+    onDecline() {
+        this.setState({ showModal: false });
+    }
+
     render() {
         return(
             <Card>
@@ -48,6 +58,8 @@ class EmployeeEdit extends Component {
                 </CardItem>
                 <Confirm
                     visible={this.state.showModal}
+                    onAccept={this.onAccept.bind(this)}
+                    onDecline={this.onDecline.bind(this)}
                 >
                     Are you sure you want to delete this?
                 </Confirm>
